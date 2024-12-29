36e4376347bdfba11a13f9aa0e98709e83ba2e59
ufukesra <ufukesra3458@gmail.com>
2024-12-29 03:20:03 +0000

    Functions

---
+++ day02/3_Functions.js
@@ -0,0 +1,24 @@
+function readings(){
+    console.log('Hello Programmers!');
+    console.log("Hello Javascript!");
+    console.log("Hello World!");
+}
+
+//readings();
+console.log("Test completed successfully!");
+
+function displayValue(value="Unknown Value"){
+    console.log("The value is: " + value);
+}
+
+displayValue(10);
+displayValue();
+
+console.log('----------------------------------');
+
+function square(number1, number2){
+    return number1 * number2;
+}
+
+let num= square(10,21);
+console.log(num);
